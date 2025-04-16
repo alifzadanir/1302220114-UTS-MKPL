2f43bdbcbd56ccdca3aea25a9f1fccb0e242c1eb
alifzadanir <alifadanir@gmail.com>
2025-04-16 18:55:32 +0700

    Refactor Long Parameter List Menggunakan Class tambahan PersonalData.java dan JoinDate.java

---
+++ src/lib/JoinDate.java
@@ -0,0 +1,17 @@
+package lib;
+
+public class JoinDate {
+    private int year;
+    private int month;
+    private int day;
+
+    public JoinDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
---
+++ src/lib/PersonalData.java
@@ -0,0 +1,13 @@
+package lib;
+
+public class PersonalData {
+    private String name;
+    private String address;
+    private String idNumber;
+
+    public PersonalData(String name, String address, String idNumber, String NPWP) {
+        this.name = name;
+        this.address = address;
+        this.idNumber = idNumber;
+    }
+}
